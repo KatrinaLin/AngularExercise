de179b2dcdb5ff04a1aff465ba3c7d5ad419222d
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-12 21:32:47 +0800

    refactor

--- src/app/lifecycle/exercise/child-b/child-b.component.ts
+++ src/app/lifecycle/exercise/child-b/child-b.component.ts
@@ -1,18 +1,33 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit} from '@angular/core';
+import {LoggerService} from "../../../shared/logger.service";
 
 @Component({
   selector: 'app-child-b',
   templateUrl: './child-b.component.html',
   styleUrls: ['./child-b.component.css']
 })
-export class ChildBComponent implements OnInit {
+export class ChildBComponent implements OnInit, OnChanges, OnDestroy {
 
   @Input()
   sharedValue = null;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public logger: LoggerService) {
+    this.logger.tick();
+    this.logger.log('=== B: Constructor ===');
   }
 
+  ngOnInit() {
+    this.logger.tick();
+    this.logger.log('=== B: ngOnInit ===');
+  }
+
+  ngOnChanges() {
+    this.logger.tick();
+    this.logger.log('=== B: ngOnChanges ===');
+  }
+
+  ngOnDestroy() {
+    this.logger.tick();
+    this.logger.log('=== B: ngOnDestroy ===');
+  }
 }
