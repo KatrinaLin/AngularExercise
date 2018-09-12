d7b87d1cbd39b854a67af8f5ef909448a0a9b9ef
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-12 16:08:01 +0800

    [Yunfei/Zhaoyan] feature: implement directive

--- src/app/directive/exercise/highlight.directive.ts
+++ src/app/directive/exercise/highlight.directive.ts
@@ -1,12 +1,23 @@
-import { Directive, ElementRef } from '@angular/core';
+import {Directive, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(el: ElementRef) {
+  constructor(public el: ElementRef) {
     el.nativeElement.style.backgroundColor = 'yellow';
   }
 
+  private setHighlightColor(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+
+  @HostListener('click') onSingleClick() {
+    this.setHighlightColor('green');
+  }
+
+  @HostListener('dblclick') onDoubleClick() {
+    this.setHighlightColor('blue');
+  }
 }
